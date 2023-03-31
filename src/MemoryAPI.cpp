2eae8dfd46cc7c6cd1e1ba1dc70b155ddf98caf4
Samanta <koushik.engine@gmail.com>
2023-03-31 21:18:24 +0530

    SDFat library finalised do not change

--- src/MemoryAPI.cpp
+++ src/MemoryAPI.cpp
@@ -1,5 +1,69 @@
 #include "MemoryAPI.h"
+char line[40];
 
+//------------------------------------------------------------------------------
+// Store error strings in flash to save RAM.
+#define error(s) sd.errorHalt(&Serial, F(s))
+//------------------------------------------------------------------------------
+// Check for extra characters in field or find minus sign.
+char *skipSpace(char *str)
+{
+    while (isspace(*str))
+        str++;
+    return str;
+}
+//------------------------------------------------------------------------------
+bool parseLine(char *str)
+{
+    char *ptr;
+
+    // Set strtok start of line.
+    str = strtok(str, ",");
+    if (!str)
+        return false;
+
+    // Print text field.
+    Serial.println(str);
+
+    // Subsequent calls to strtok expects a null pointer.
+    str = strtok(nullptr, ",");
+    if (!str)
+        return false;
+
+    // Convert string to long integer.
+    int32_t i32 = strtol(str, &ptr, 0);
+    if (str == ptr || *skipSpace(ptr))
+        return false;
+    Serial.println(i32);
+
+    str = strtok(nullptr, ",");
+    if (!str)
+        return false;
+
+    // strtoul accepts a leading minus with unexpected results.
+    if (*skipSpace(str) == '-')
+        return false;
+
+    // Convert string to unsigned long integer.
+    uint32_t u32 = strtoul(str, &ptr, 0);
+    if (str == ptr || *skipSpace(ptr))
+        return false;
+    Serial.println(u32);
+
+    str = strtok(nullptr, ",");
+    if (!str)
+        return false;
+
+    // Convert string to double.
+    double d = strtod(str, &ptr);
+    if (str == ptr || *skipSpace(ptr))
+        return false;
+    Serial.println(d);
+
+    // Check for extra fields.
+    return strtok(nullptr, ",") == nullptr;
+}
+//------------------------------------------------------------------------------
 MemoryAPI::MemoryAPI() {}
 void MemoryAPI::setup()
 {
@@ -11,6 +75,8 @@ void MemoryAPI::setup()
         delay(1000);
     }
     IS_LOG_ENABLED ? Serial.println(F("SD initialization success.")) : 0;
+
+    IS_LOG_ENABLED ? sd.ls("/", LS_R) : 0;
 }
 
 void MemoryAPI::errorPrint()
